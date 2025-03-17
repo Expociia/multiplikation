283be57c5d5f7b7048aec3b1438386d5a3c356ee
Expociia <martin.birgerson@gmail.com>
2025-03-17 11:20:04 +0100

    Flyttar Firebase-konfiguration till separat config-fil för bättre säkerhet

---
+++ config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY || "din_api_nyckel_här",
+    authDomain: "multiplikationstraning.firebaseapp.com",
+    projectId: "multiplikationstraning",
+    storageBucket: "multiplikationstraning.appspot.com",
+    messagingSenderId: "428828316384",
+    appId: "1:428828316384:web:3a7552b2f4c255be01b685",
+    measurementId: "G-RQBQ8R59HM"
+};
+
+export default firebaseConfig; 
